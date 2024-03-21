3155d6ae514286b9c8774283cf40ee65f2c23452
Eksaayl <144189773+Eksaayl@users.noreply.github.com>
2024-03-21 22:16:21 +0800

    Inquiry responsive w/ FAQ updated

--- YourStudioFinal/obj/Release/net8.0/YourStudioFinal.AssemblyInfo.cs
+++ YourStudioFinal/obj/Release/net8.0/YourStudioFinal.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db1f59dbb3299cc47ab3904972ce38a5dc929b71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a1d96c11e5f47959ba00ddb6f4b03f3ae65ece5")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
