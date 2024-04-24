70ee8b95f0e690ee3ddf9b3e29c9dad600540d8c
emsheesh69 <ejmntclr22@gmail.com>
2024-04-25 05:24:21 +0800

    fixed navbar login reg

--- YourStudioFinal/obj/Debug/net8.0/YourStudioFinal.AssemblyInfo.cs
+++ YourStudioFinal/obj/Debug/net8.0/YourStudioFinal.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13eb389cdd023bb16ec9a57050005c4f9d3530a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ddf8dc90cf2bed2c4839966049bb3ae018efa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
