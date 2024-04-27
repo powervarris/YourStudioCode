9502a521f304e3ba24a9828d675e16a40a5d3245
powervarris <kyaasenpai123@gmail.com>
2024-04-27 12:01:35 +0800

    fixed issues

--- YourStudioFinal/obj/Debug/net8.0/YourStudioFinal.AssemblyInfo.cs
+++ YourStudioFinal/obj/Debug/net8.0/YourStudioFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc167e27ae095fa8413524e4c4f417d0067f5bda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourStudioFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
